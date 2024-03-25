2c92fde312652f510481fdf740e7efefc1c487cb
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-03-24 22:52:32 -0500

    feat: return jinja template on base endpoint

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+import pathlib
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
 
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
-@app.get("/")
-def home_view():
-    return {"text": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
-@app.get("/")
+@app.post("/")
 def home_detail_view():
     return {"text": "Hello World"}
